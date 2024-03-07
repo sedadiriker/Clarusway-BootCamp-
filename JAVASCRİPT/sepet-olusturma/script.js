6968ea70b66b1bd7726e6e8d62629ea5feb618ad
Seda Diriker <146026945+sedadiriker@users.noreply.github.com>
2024-03-07 15:14:44 +0300

    sepet-olusturma fixes

--- JAVASCRİPT/sepet-olusturma/script.js
+++ JAVASCRİPT/sepet-olusturma/script.js
@@ -7,109 +7,127 @@ let sepet_toplam = document.querySelector('.sepet-toplamı')
 let adet_toplamı = document.querySelector('.adet-toplamı')
 const toplamDelete = document.querySelector('toplam-delete')
 const count = document.getElementById('count')
+const sepet_onay = document.querySelector('.btn-onay')
 
 
-const stok = [
-    {
-        ürünAdı : 'Çubuklu Forma',
-        stokAdet : 5
-    },
-    {
-        ürünAdı : 'Sarı Baby Set',
-        stokAdet : 4
-    },
-    {
-        ürünAdı : 'Logo Şapka',
-        stokAdet : 3
-    },
-    {
-        ürünAdı : 'Kol saati',
-        stokAdet : 2
-    },
 
+let stok = [
+    {
+        ürünAdı: 'Çubuklu Forma',
+        stokAdet: 5
+    },
+    {
+        ürünAdı: 'Sarı Baby Set',
+        stokAdet: 4
+    },
+    {
+        ürünAdı: 'Logo Şapka',
+        stokAdet: 3
+    },
+    {
+        ürünAdı: 'Kol saati',
+        stokAdet: 2
+    },
 ]
-    
-
+localStorage.setItem('stok', JSON.stringify(stok))
 
 let sepetUrunler = []
-let adetTd //! Globale almam gerekti, arttırırken güncellemek için.
-let fiyatTd
 let option
+let ayniUrunIndex;
 
 sepeteEkleBtns.forEach(sepeteEkle => {
     sepeteEkle.addEventListener('click', (e) => {
-        
+        option = parseInt(e.target.closest('.card').querySelector('#adet').value)
+
         const ürünBilgileri = {
             id: new Date().getTime(),
-            ürünAdı : e.target.closest('.card').querySelector('.card-title').textContent,
+            ürünAdı: e.target.closest('.card').querySelector('.card-title').textContent,
             fiyat: parseFloat((e.target.closest('.card').querySelector('.card-price').textContent).replace('₺', '').replace('.', '').replace(',', '.')),
-            img : e.target.closest('.card').querySelector('img').getAttribute('src'),
-            adet : 1
+            img: e.target.closest('.card').querySelector('img').getAttribute('src'),
+            adet: option
         }
-       
-        //!option değerine göre ekleme
-        option = e.target.closest('.card').querySelector('#adet')
 
-        let ayniUrunIndex = sepetUrunler.findIndex(ürün =>
-            ürün.ürünAdı === ürünBilgileri.ürünAdı) //! aynı ürün varsa ilgili indeksi döndürür
-        if(option) {
-            //! üründen aynı var mı kontrol
-            if(ayniUrunIndex !== -1) {
-                adetTd.textContent = sepetUrunler[ayniUrunIndex].adet * parseInt(option.value) + parseInt(adetTd.textContent)
-                console.log(adetTd)
-    
-                fiyatTd.textContent = (ürünBilgileri.fiyat * parseInt(adetTd.textContent)).toFixed(2)
+        stok.forEach(ürün => {
+            if (ürün.ürünAdı == ürünBilgileri.ürünAdı) {
+                if(ürün.stokAdet == 0) {
+                    alert('ürün tükendi')
+                }else{
+                    if (option > ürün.stokAdet) {
+                        alert(`En fazla ${ürün.stokAdet} adet ekleyebilirsiniz`)
+                        return
+                    }else{
+                        const ayniUrunIndex = sepetUrunler.findIndex(ürün => ürün.ürünAdı === ürünBilgileri.ürünAdı)
+                        
+                            if (ayniUrunIndex !== -1) {
+                                const adetTd = document.getElementById('adet_' + sepetUrunler[ayniUrunIndex].id)
+                                adetTd.textContent = parseInt(adetTd.textContent) + option
+                                if(adetTd.textContent > ürün.stokAdet) {
+                                    alert(`En fazla ${ürün.stokAdet} adet ekleyebilirsiniz`)
+                                    adetTd.textContent = parseInt(adetTd.textContent) - option
+                                    return
+                                }else{
+                                    const fiyatTd = document.getElementById('fiyat_' + sepetUrunler[ayniUrunIndex].id)
+                                    fiyatTd.textContent = (ürünBilgileri.fiyat * parseInt(adetTd.textContent)).toFixed(2)
+                    
+                                }
+                                
+                            } else {
+                                DOM(ürünBilgileri)
+                                sepetUrunler.push(ürünBilgileri)
+                            }
+                        
+                    }
+                }
                 
-            } else {
-                DOM(ürünBilgileri);
-                sepetUrunler.push(ürünBilgileri);
             }
-        }
+
         
-       option.value = '1'
-    //    adet_toplamı.textContent = toplamlar()
-    toplamlar()
+        })
 
 
-    
-    }
-)})
+
+
+        option = 1
+        toplamlar()
+        localStorage.setItem("sepet", JSON.stringify(sepetUrunler))
+    })
+})
+
 
 //! Dom a yazma
-const DOM = ({id,ürünAdı,fiyat,img,adet}) => {
-    
+const DOM = ({ id, ürünAdı, fiyat, img, adet }) => {
     const tr = document.createElement("tr")
-
-    const appendTd = (content)=>{
+    const appendTd = (content) => {
         const td = document.createElement("td")
-        td.textContent = content;
+        td.textContent = content
         return td
-        }
-    
+    }
+
     //Resim
-    const imageTd = document.createElement('td') 
-    imageTd.classList.add('class', 'w-25')   
-    const image = document.createElement('img')
-    image.classList.add('img-fluid')
-    imageTd.appendChild(image)
-    image.setAttribute('src',`${img}`)
-    
+    const imageTd = document.createElement('td');
+    imageTd.classList.add('class', 'w-25');
+    const image = document.createElement('img');
+    image.classList.add('img-fluid');
+    imageTd.appendChild(image);
+    image.setAttribute('src', `${img}`);
 
     //çöp sepeti
-    const çöpSepetiTd = document.createElement('td')    
-    const çöpSepeti = document.createElement('i')
-    çöpSepeti.id = id
-    çöpSepeti.classList.add( 'fas','fa-trash-can','remove')
-    çöpSepetiTd.appendChild(çöpSepeti)
-    
+    const çöpSepetiTd = document.createElement('td');
+    const çöpSepeti = document.createElement('i');
+    çöpSepeti.id = id;
+    çöpSepeti.classList.add('fas', 'fa-trash-can', 'remove');
+    çöpSepetiTd.appendChild(çöpSepeti);
+
     //adet
-    adetTd = document.createElement('td')
-    adetTd.textContent = adet * parseInt(option.value)
-    adetTd.style.textAlign = 'center'
+    const adetTd = document.createElement('td');
+    adetTd.textContent = adet;
+    adetTd.style.textAlign = 'center';
+    adetTd.id = 'adet_' + id;
 
     //fiyat
-    fiyatTd = document.createElement('td')
-    fiyatTd.textContent = (fiyat * parseInt(option.value)).toFixed(2)
+    const fiyatTd = document.createElement('td');
+    fiyatTd.textContent = (fiyat * adet).toFixed(2);
+    fiyatTd.id = 'fiyat_' + id;
 
     //satıra ekle
     tr.append(
@@ -118,81 +136,95 @@ const DOM = ({id,ürünAdı,fiyat,img,adet}) => {
         adetTd,
         fiyatTd,
         çöpSepetiTd
-    )
-    tr.id = id
-    tbody.prepend(tr)
-    
-}
+    );
+    tr.id = id;
+    tbody.prepend(tr);
+};
 
 //!sepet silme iconu dinleme
 tbody.addEventListener('click', (e) => {
-
     if (e.target.classList.contains('toplam-delete')) {
-        confirm('Sepeti boşaltmak istediğinizden emin misiniz?')
-        if(confirm) {
-            const tumTrler = document.querySelectorAll('tbody tr')
+        confirm('Sepeti boşaltmak istediğinizden emin misiniz?');
+        if (confirm) {
+            const tumTrler = document.querySelectorAll('tbody tr');
             for (let i = 0; i < tumTrler.length - 1; i++) {
-                tumTrler[i].remove()
-                sepetUrunler.length = 0
-                // console.log(sepetUrunler);
+                tumTrler[i].remove();
+                sepetUrunler.length = 0;
             }
             adet_toplamı.textContent = ''
             sepet_toplam.textContent = ''
-            window.location.reload();
+            window.location.reload()
+            localStorage.removeItem('sepet')
         }
-    }else if(e.target.classList.contains('remove')){
+    } else if (e.target.classList.contains('remove')) {
         e.target.parentElement.parentElement.remove()
-        // console.log(e.target.id);
-
-        //! sepetten aynı id ye sahip ürünü bulup kaldırma
-        const id = e.target.id 
-        sepetUrunler = sepetUrunler.filter((ürün)=> ürün.id != id)
-
-        //!Siinen tutarı toplamdan düşme
+        const id = e.target.id
+        sepetUrunler = sepetUrunler.filter((ürün) => ürün.id != id)
         const silinenTutar = Number((e.target.parentElement.parentElement).children[3].textContent)
-        // console.log(sepet_toplam);
         sepet_toplam.textContent = (Number(sepet_toplam.textContent) - silinenTutar).toFixed(2)
-        // console.log(silinenTutar);
-
-        //!silinen adeti güncelleme
         const silinenAdet = Number((e.target.parentElement.parentElement).children[2].textContent)
         adet_toplamı.textContent -= silinenAdet
-        if(tbody.children.length == 1) {
-            window.location.reload();
+        localStorage.setItem('sepet', JSON.stringify(sepetUrunler))
+
+        if (tbody.children.length == 1) {
+            window.location.reload()
         }
     }
-    
+    toplamlar()
 })
 
-
 //!Sepete tıklama
-sepetim.addEventListener('click', () =>{
-    
-    if(tbody.children.length > 1) {
+sepetim.addEventListener('click', () => {
+    if (tbody.children.length > 1) {
         table.classList.toggle('hidden')
-    }else{
+    } else {
         table.classList.add('hidden')
-    } 
+    }
 })
 
 //!  toplamları hesaplama
 const toplamlar = () => {
-    //!adet toplamı hesaplama   &&    //! tutar toplamı hesaplama
-
     const adetler = document.querySelectorAll('tbody tr')
-
     let toplamAdet = 0
     let toplamTutar = 0
-    for(let i = 0; i < adetler.length - 1; i ++) {
+    for (let i = 0; i < adetler.length - 1; i++) {
         toplamAdet += Number(adetler[i].children[2].textContent)
         toplamTutar += Number(adetler[i].children[3].textContent)
     }
+    // console.log(toplamAdet)
     adet_toplamı.textContent = toplamAdet
     adet_toplamı.style.textAlign = 'center'
-
     sepet_toplam.textContent = toplamTutar.toFixed(2)
     count.textContent = toplamAdet
 }
 
+//!sayfa yüklendiğinde...
+window.addEventListener("load", () => {
+    sepetUrunler = JSON.parse(localStorage.getItem("sepet")) || []
+    // console.log(sepetUrunler)
+    sepetUrunler.forEach(ürün => {
+        DOM(ürün)
+    })
+    toplamlar()
+    stok = JSON.parse(localStorage.getItem('stok')) || [] //! atamayı unutma!!!!
+    console.log(stok);
+})
 
+//! ürün alma
+sepet_onay.addEventListener('click', () => {
+    stok.forEach(ürün => {
+        sepetUrunler.forEach(sepetürün => {
+            if(sepetürün.ürünAdı == ürün.ürünAdı) {
+                ürün.stokAdet -= sepetürün.adet
+                console.log(stok)
+                sepetUrunler = []
+                localStorage.setItem('stok', JSON.stringify(stok))
+                localStorage.setItem('sepet', JSON.stringify(sepetUrunler))
+                table.innerHTML = ''
+                alert('iyi günlerde kullanın')
+                window.location.reload()
+            }
+        })
+    })
+})
 
