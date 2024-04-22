2d8a197c837842256e28c27636f8113e463e4ce5
Seda Diriker <146026945+sedadiriker@users.noreply.github.com>
2024-04-22 19:03:39 +0300

    add contextApi

---
+++ REACT/InClass/contextApi-inclass/src/pages/PrivateRouter.jsx
@@ -0,0 +1,10 @@
+import { useContext } from "react"
+import { Outlet, Navigate } from "react-router-dom"
+import { LoginContext } from "../context/LoginContext"
+
+const PrivateRouter = () => {
+  const {signed} = useContext(LoginContext)
+  return signed ? <Outlet /> : <Navigate to="/login" />
+}
+
+export default PrivateRouter
